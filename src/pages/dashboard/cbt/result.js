5a5b914d5571a203f820462b75a120ea33748d06
manaim2112 <m.a.naim2112@gmail.com>
2023-05-24 16:59:34 +0700

    add kehadiran

--- src/pages/dashboard/cbt/result.js
+++ src/pages/dashboard/cbt/result.js
@@ -6,7 +6,7 @@ import { getStudent } from "../../../service/dashboard/users"
 import { RemoveResultWithId, getDataWithIdCBT, getWithIdCBT } from "../../../service/dashboard/cbt"
 import { SkeletonTable } from "../../../elements/skeleton/table"
 import { Suspense } from "react"
-import { Chip, IconButton, Typography } from "@material-tailwind/react"
+import { Chip, IconButton, Tooltip, Typography } from "@material-tailwind/react"
 import Swal from "sweetalert2"
 import jsPDF from "jspdf"
 import { JSONParse } from "../../../service/constant"
@@ -27,6 +27,7 @@ export function ResultCBT() {
     const HtmlRef = useRef(null)
     const [load, setLoad] = useState(false)
     const [loadExportToExcel, setLoadExportToExcel] = useState(false)
+    const [loadExportToExcelKehadiran, setLoadExportToExcelKehadiran] = useState(false)
 
     useEffect(() => {
         processData()
@@ -182,6 +183,47 @@ export function ResultCBT() {
         })
         // writeXLSX
     }
+    const exportToExcelKehadiran = () => {
+        setLoadExportToExcelKehadiran(true)
+        const ll = list.tokelas.split(",").map(e => e.trim())
+        getUserWithKelas(ll).then(response => {
+            const workbook =  XLSX.utils.book_new()
+            response.forEach((user, keyUser) => {
+                // End score
+                const rows = [];
+                console.log(user)
+                user.forEach((u, k) => {
+                    const ind = result.findIndex(Obj => Obj.iduser === u.id)
+                    if(ind === -1) return;
+                    console.log(result[ind])
+                    rows.push({
+                        No : k+1,
+                        Nisn : u.nisn,
+                        Nama : u.name,
+                        Kelas : u.kelas,
+                        KEHADIRAN : status(u.id) === "start" ? "HADIR" : status(u.id) === "finish" ? "HADIR" : "HADIR",
+                        Masuk : atob(result[ind].created_at ? result[ind].created_at : ""),
+                        // Selesai : atob(result[ind].updated_at)
+                    })
+                })
+                const worksheet = XLSX.utils.json_to_sheet(rows);
+                var wscols = [
+                    {wch:3},
+                    {wch:13},
+                    {wch:25},
+                    {wch:3},
+                    {wch:10},
+                    {wch:20},
+                ];
+                
+                worksheet['!cols'] = wscols;
+                XLSX.utils.book_append_sheet(workbook, worksheet, ll[keyUser])
+            })
+            XLSX.writeFile(workbook, list.name + "_RekapKehadiran_"+ Date.now() +".xlsx", { compression: true });
+            setLoadExportToExcelKehadiran(false)
+        })
+        // writeXLSX
+    }
 
     const resetResult = (id) => {
         const index = result.findIndex(Obj => Obj.iduser === id)
@@ -281,83 +323,133 @@ export function ResultCBT() {
             load ? (
                 <SkeletonTable/>
             ) : (
-                <table ref={HtmlRef} className="w-full text-sm text-left text-black dark:text-gray-400 mt-4 print">
-                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th scope="col" className="px-6 py-3">
-                                    Absen
-                                </th>
-                                <th scope="col" className="px-6 py-3">
-                                    Nama
-                                </th>
-                                <th scope="col" className="px-6 py-3">
-                                    Kelas
-                                </th>
-                                <th scope="col" className="px-6 py-3">
-                                    Progress
-                                </th>
-                                <th scope="col" className="px-6 py-3">
-                                    Nilai
-                                </th>
-                                <th scope="col" className="px-6 py-3">
-                                    cek hasil
-                                </th>
-                                <th scope="col" className="px-6 py-3">
-                                    
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {
-                            user.map((e,k) => (
-                                <tr key={k} className={`bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 ${k % 19 === 0 ? "page-break" : ""}`}>
-                                    <td className="px-6 py-1">{k+1}</td>
-                                    <td className="px-6 py-1">{e.name}</td>
-                                    <td className="px-6 py-1">{e.kelas}</td>
-                                    <td className="px-6 py-1">{
-                                        status(e.id) === "start" ? (
-                                            <Chip color="orange" value={"Sedang mengerjakan"}/>
-                                        ) : status(e.id) === "finish" ? (
-                                            <Chip color="green" value={"Tuntas"}/>
-                                        ) : (
-                                            <>
-                                                <Chip color="gray" value={"Belum Absen"}/>
-                                            </>
-                                        )
-                                    }</td>
-                                    <td className="px-6 py-1">{score[k]}</td>
-
-                                    <td>
-                                        {
-                                            status(e.id) === "finish" ? (
-                                                <Link className="text-blue-400 hover:text-blue-600 hover:underline" to={"/dashboard/cbt/id/"+ id + "/result/"+ e.id +"/view"}>Cek Hasil</Link>
-                                            ) : "Cek Hasil"
-                                        }
-                                        <span className="ml-3 text-red-400 hover:text-red-600 hover:underline" onClick={() => resetResult(e.id)}> Reset </span>
-                                    </td>
+                <div className="relative overflow-x-auto">
+                    <table ref={HtmlRef} className="w-full text-sm text-left text-black dark:text-gray-400 mt-4 print">
+                            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" className="px-6 py-3">
+                                        Absen
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        Nama
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        Kelas
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        Progress
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        Nilai
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        cek hasil
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        
+                                    </th>
                                 </tr>
-                            ))
-                        }
-                        </tbody>
-                </table>
+                            </thead>
+                            <tbody>
+                            {
+                                user.map((e,k) => (
+                                    <tr key={k} className={`bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 ${k % 19 === 0 ? "page-break" : ""}`}>
+                                        <td className="px-6 py-1">{k+1}</td>
+                                        <td className="px-6 py-1">{e.name}</td>
+                                        <td className="px-6 py-1">{e.kelas}</td>
+                                        <td className="px-6 py-1">{
+                                            status(e.id) === "start" ? (
+                                                <Chip color="orange" value={"Sedang mengerjakan"}/>
+                                            ) : status(e.id) === "finish" ? (
+                                                <Chip color="green" value={"Tuntas"}/>
+                                            ) : (
+                                                <>
+                                                    <Chip color="gray" value={"Belum Absen"}/>
+                                                </>
+                                            )
+                                        }</td>
+                                        <td className="px-6 py-1">{score[k]}</td>
+
+                                        <td>
+                                            {
+                                                status(e.id) === "finish" ? (
+                                                    <Link className="text-blue-400 hover:text-blue-600 hover:underline" to={"/dashboard/cbt/id/"+ id + "/result/"+ e.id +"/view"}>Cek Hasil</Link>
+                                                ) : "Cek Hasil"
+                                            }
+                                            <span className="ml-3 text-red-400 hover:text-red-600 hover:underline" onClick={() => resetResult(e.id)}> Reset </span>
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+                            </tbody>
+                    </table>
+                </div>
             )
         }
 
-        <div className="fixed z-50 bottom-5 right-5">
-            <IconButton onClick={exportToExcel} color="green" variant="gradient" className="ml-3" disabled={loadExportToExcel}>
-                {
-                    loadExportToExcel ? (
-                        <SignalIcon className="w-4 h-4 animate-spin"></SignalIcon>
-                    ) : (
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-filetype-xlsx" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M14 4.5V11h-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5ZM7.86 14.841a1.13 1.13 0 0 0 .401.823c.13.108.29.192.479.252.19.061.411.091.665.091.338 0 .624-.053.858-.158.237-.105.416-.252.54-.44a1.17 1.17 0 0 0 .187-.656c0-.224-.045-.41-.135-.56a1.002 1.002 0 0 0-.375-.357 2.028 2.028 0 0 0-.565-.21l-.621-.144a.97.97 0 0 1-.405-.176.37.37 0 0 1-.143-.299c0-.156.061-.284.184-.384.125-.101.296-.152.513-.152.143 0 .266.023.37.068a.624.624 0 0 1 .245.181.56.56 0 0 1 .12.258h.75a1.093 1.093 0 0 0-.199-.566 1.21 1.21 0 0 0-.5-.41 1.813 1.813 0 0 0-.78-.152c-.293 0-.552.05-.777.15-.224.099-.4.24-.527.421-.127.182-.19.395-.19.639 0 .201.04.376.123.524.082.149.199.27.351.367.153.095.332.167.54.213l.618.144c.207.049.36.113.462.193a.387.387 0 0 1 .153.326.512.512 0 0 1-.085.29.558.558 0 0 1-.255.193c-.111.047-.25.07-.413.07-.117 0-.224-.013-.32-.04a.837.837 0 0 1-.249-.115.578.578 0 0 1-.255-.384h-.764Zm-3.726-2.909h.893l-1.274 2.007 1.254 1.992h-.908l-.85-1.415h-.035l-.853 1.415H1.5l1.24-2.016-1.228-1.983h.931l.832 1.438h.036l.823-1.438Zm1.923 3.325h1.697v.674H5.266v-3.999h.791v3.325Zm7.636-3.325h.893l-1.274 2.007 1.254 1.992h-.908l-.85-1.415h-.035l-.853 1.415h-.861l1.24-2.016-1.228-1.983h.931l.832 1.438h.036l.823-1.438Z"/>
-                            </svg>
-                    )
-                }
-            </IconButton>
-            <IconButton onClick={handleSaveAsPDF} color="red" variant="gradient" className="ml-3">
-                <PrinterIcon className="w-4 h-4"/>
-            </IconButton>
+        <div className="fixed z-[500] bottom-5 right-5">
+            <div className="mt-3">
+                <Tooltip
+                    content="Cetak kehadiran"
+                    animate={{
+                        mount: { scale: 1, y: 0 },
+                        unmount: { scale: 0, y: 25 },
+                    }}
+                    placement="left-end"
+                    >
+                    <IconButton onClick={exportToExcelKehadiran} color="yellow" variant="gradient" className="ml-3" disabled={loadExportToExcelKehadiran}>
+                        {
+                            loadExportToExcelKehadiran ? (
+                                <SignalIcon className="w-4 h-4 animate-spin"></SignalIcon>
+                            ) : (
+                                    <div className="flex gap-2">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-filetype-xlsx" viewBox="0 0 16 16">
+                                            <path fillRule="evenodd" d="M14 4.5V11h-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5ZM7.86 14.841a1.13 1.13 0 0 0 .401.823c.13.108.29.192.479.252.19.061.411.091.665.091.338 0 .624-.053.858-.158.237-.105.416-.252.54-.44a1.17 1.17 0 0 0 .187-.656c0-.224-.045-.41-.135-.56a1.002 1.002 0 0 0-.375-.357 2.028 2.028 0 0 0-.565-.21l-.621-.144a.97.97 0 0 1-.405-.176.37.37 0 0 1-.143-.299c0-.156.061-.284.184-.384.125-.101.296-.152.513-.152.143 0 .266.023.37.068a.624.624 0 0 1 .245.181.56.56 0 0 1 .12.258h.75a1.093 1.093 0 0 0-.199-.566 1.21 1.21 0 0 0-.5-.41 1.813 1.813 0 0 0-.78-.152c-.293 0-.552.05-.777.15-.224.099-.4.24-.527.421-.127.182-.19.395-.19.639 0 .201.04.376.123.524.082.149.199.27.351.367.153.095.332.167.54.213l.618.144c.207.049.36.113.462.193a.387.387 0 0 1 .153.326.512.512 0 0 1-.085.29.558.558 0 0 1-.255.193c-.111.047-.25.07-.413.07-.117 0-.224-.013-.32-.04a.837.837 0 0 1-.249-.115.578.578 0 0 1-.255-.384h-.764Zm-3.726-2.909h.893l-1.274 2.007 1.254 1.992h-.908l-.85-1.415h-.035l-.853 1.415H1.5l1.24-2.016-1.228-1.983h.931l.832 1.438h.036l.823-1.438Zm1.923 3.325h1.697v.674H5.266v-3.999h.791v3.325Zm7.636-3.325h.893l-1.274 2.007 1.254 1.992h-.908l-.85-1.415h-.035l-.853 1.415h-.861l1.24-2.016-1.228-1.983h.931l.832 1.438h.036l.823-1.438Z"/>
+                                        </svg>
+                                        <div> </div>
+                                    </div>
+                            )
+                        }
+                    </IconButton>
+
+                </Tooltip>
+            </div>
+            <div className="mt-3">
+            <Tooltip
+                    content="Cetak Nilai"
+                    animate={{
+                        mount: { scale: 1, y: 0 },
+                        unmount: { scale: 0, y: 25 },
+                    }}
+                    placement="left-end"
+                    >
+                <IconButton onClick={exportToExcel} color="green" variant="gradient" className="ml-3" disabled={loadExportToExcel}>
+                    {
+                        loadExportToExcel ? (
+                            <SignalIcon className="w-4 h-4 animate-spin"></SignalIcon>
+                        ) : (
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-filetype-xlsx" viewBox="0 0 16 16">
+                                    <path fillRule="evenodd" d="M14 4.5V11h-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5ZM7.86 14.841a1.13 1.13 0 0 0 .401.823c.13.108.29.192.479.252.19.061.411.091.665.091.338 0 .624-.053.858-.158.237-.105.416-.252.54-.44a1.17 1.17 0 0 0 .187-.656c0-.224-.045-.41-.135-.56a1.002 1.002 0 0 0-.375-.357 2.028 2.028 0 0 0-.565-.21l-.621-.144a.97.97 0 0 1-.405-.176.37.37 0 0 1-.143-.299c0-.156.061-.284.184-.384.125-.101.296-.152.513-.152.143 0 .266.023.37.068a.624.624 0 0 1 .245.181.56.56 0 0 1 .12.258h.75a1.093 1.093 0 0 0-.199-.566 1.21 1.21 0 0 0-.5-.41 1.813 1.813 0 0 0-.78-.152c-.293 0-.552.05-.777.15-.224.099-.4.24-.527.421-.127.182-.19.395-.19.639 0 .201.04.376.123.524.082.149.199.27.351.367.153.095.332.167.54.213l.618.144c.207.049.36.113.462.193a.387.387 0 0 1 .153.326.512.512 0 0 1-.085.29.558.558 0 0 1-.255.193c-.111.047-.25.07-.413.07-.117 0-.224-.013-.32-.04a.837.837 0 0 1-.249-.115.578.578 0 0 1-.255-.384h-.764Zm-3.726-2.909h.893l-1.274 2.007 1.254 1.992h-.908l-.85-1.415h-.035l-.853 1.415H1.5l1.24-2.016-1.228-1.983h.931l.832 1.438h.036l.823-1.438Zm1.923 3.325h1.697v.674H5.266v-3.999h.791v3.325Zm7.636-3.325h.893l-1.274 2.007 1.254 1.992h-.908l-.85-1.415h-.035l-.853 1.415h-.861l1.24-2.016-1.228-1.983h.931l.832 1.438h.036l.823-1.438Z"/>
+                                </svg>
+                        )
+                    }
+                </IconButton>
+            </Tooltip>
+            </div>
+            <div className="mt-3">
+            <Tooltip
+                    content="Simpan ke Pdf"
+                    animate={{
+                        mount: { scale: 1, y: 0 },
+                        unmount: { scale: 0, y: 25 },
+                    }}
+                    placement="left-end"
+                    >
+                    <IconButton onClick={handleSaveAsPDF} color="red" variant="gradient" className="ml-3">
+                        <PrinterIcon className="w-4 h-4"/>
+                    </IconButton>
+                </Tooltip>
+            </div>
         </div>
         </Suspense>
     )
